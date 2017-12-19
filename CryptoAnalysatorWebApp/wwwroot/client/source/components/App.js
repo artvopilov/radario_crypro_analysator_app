f0bfea22087b04cfbf6963eda1545fba3289412e
Artyom <mister.0025@yandex.ru>
2017-12-19 19:46:21 +0300

    React.js client in development

--- CryptoAnalysatorWebApp/wwwroot/client/source/components/App.js
+++ CryptoAnalysatorWebApp/wwwroot/client/source/components/App.js
@@ -1,27 +1,46 @@
 const React = require('react');
+const axios = require('axios');
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            crosses: [],
+            pairs: []
+        };
 
-        }
+
+    }
+
+    componentWillMount() {
+        axios.get('/api/actualpairs')
+            .then((response) => {
+                this.state.pairs = response.data.pairs;
+                console.log(this.state.pairs[0].pair)
+            });
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                {this.state.pairs.forEach(pair => {
+                    return (
+                        <div>
+                            {pair.pair}
+                        </div>
+                    )
+                })}
+            </div>
+        )
     }
 }
 
-/** Функция вызывает при успешной транзакции
-*!/
-onTransaction() {
-    axios.get('/cards').then(({data}) => {
-        const cardsList = App.prepareCardsData(data);
-        this.setState({cardsList});
 
-        axios.get('/transactions').then(({data}) => {
-            const cardHistory = App.prepareHistory(cardsList, data);
-            this.setState({cardHistory});
-        });
-    }
-});*/
 
 module.exports = App;
